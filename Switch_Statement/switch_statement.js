b7191abe41ae45c988254b76238483fd8727f870
Elippsis007 <michaeldrmm@gmail.com>
2022-11-20 18:44:43 +0000

    completed switch statements lesson

---
+++ Switch_Statement/switch_statement.js
@@ -0,0 +1,27 @@
+/* In the example I will have a weekday variable and for each day there is a different activity
+    mapping one activity to each day */
+
+const day = 'wednesday';
+
+switch(day) {
+    case 'monday': // day === 'monday' and if this is true then the below will be executed
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Go to gym');
+        break;
+    case 'wednesday':
+        console.log('Driving lessons');
+        break;
+    case 'thursday':
+    case 'friday':
+        console.log('Work');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Not working');
+        break;
+    default:
+        console.log('Not a valid day!')
+}
