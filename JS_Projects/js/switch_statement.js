d025a07ae06cb92c73ead413c77e7473c6042073
Elippsis007 <michaeldrmm@gmail.com>
2022-11-20 19:16:24 +0000

    added new html page for other projects

---
+++ JS_Projects/js/switch_statement.js
@@ -0,0 +1,19 @@
+let msg; // Message
+let level = 1; // Level
+
+// Determine message based on level
+switch(level) {
+    case 1:
+        msg = 'Good luck on your first test';
+        break;
+    case 2:
+        msg = 'Second of three - Keep Going!';
+        break;
+    case 3:
+        msg = 'Final round, almost there';
+        break;
+    default:
+        msg = 'Good Luck';
+        break;
+}
+console.log(1)
