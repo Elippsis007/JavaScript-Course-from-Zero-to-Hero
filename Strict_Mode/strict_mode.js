eb132d53387cc734c913799985381f473a6541ad
Elippsis007 <michaeldrmm@gmail.com>
2022-12-03 21:11:43 +0000

    finished strict mode lesson

---
+++ Strict_Mode/strict_mode.js
@@ -0,0 +1,14 @@
+// Strict mode is a special mode in JS that allws people  to make it easier to write secure JS code
+// To enable "Strict Mode" for JS I need to type 'use strict'; at the top of each .js script file
+
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive!');
+
+// const interface = 'Audio';
+
+// const private = 534;
