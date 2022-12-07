e8eb56217e9c78da913214e19cbcb62c72ca5ab1
Elippsis007 <michaeldrmm@gmail.com>
2022-12-07 17:33:30 +0000

    finished functions lessons

--- Functions/functions.js
+++ Functions/functions.js
@@ -18,10 +18,13 @@ logger();
 
 // Naming the function "fruitProcessor" with parenthesis containg two parameters
 function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
+    //console.log(apples, oranges);
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
     return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
